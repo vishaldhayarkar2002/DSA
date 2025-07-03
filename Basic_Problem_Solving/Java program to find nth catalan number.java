94c467bba91e36904fd6b46aa92d6c2c6d88d7b2
Vishal <vishaldhayarkar2002@gmail.com>
2025-07-03 17:06:10 +0530

    remove comments

--- Basic_Problem_Solving/Java program to find nth catalan number.java
+++ Basic_Problem_Solving/Java program to find nth catalan number.java
@@ -4,14 +4,11 @@ class GfG {
 
     static int findCatalan(int n) {
       
-        // Table to store results of subproblems
         int[] catalan = new int[n + 1];
+        
 
-        // Initialize first two values in the table
         catalan[0] = catalan[1] = 1;
 
-        // Fill entries in catalan[] using the recursive
-        // formula
         for (int i = 2; i <= n; i++) {
             catalan[i] = 0;
             for (int j = 0; j < i; j++) {
@@ -20,7 +17,6 @@ class GfG {
             }
         }
 
-        // Return the last entry
         return catalan[n];
     }
 
