d941f14c59e1c18dee7a2e10d77491913624b704
Vishal <vishaldhayarkar2002@gmail.com>
2025-08-17 23:05:16 +0530

    Started solving basic problems

---
+++ Basic_Problem_Solving/check_prime_or_not.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        boolean isPrime = true;
+        
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        
+        System.out.println(num + (isPrime ? " is a prime number." : " is not a prime number."));
+    }
+}
