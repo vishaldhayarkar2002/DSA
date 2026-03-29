4ff867e16c5bfc79bdea9e53b6e16af6e516e639
Vishal <vishal.dhayarkar@erpdata.in>
2026-03-29 20:34:47 +0530

    till searching done of 2D matrix

--- TCS_pratice_basic_imp/imp.java
+++ TCS_pratice_basic_imp/imp.java
@@ -90,3 +90,30 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+//Search a 2D Matrix -- it is 
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+
+    int left = 0, right = m * n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        int row = mid / n; // this is the 
+        int col = mid % n; //important part
+
+        if (matrix[row][col] == target) {
+            return true;
+        } else if (matrix[row][col] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
+}
